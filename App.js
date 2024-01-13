b0ff341805daedfa34e318771d875b93deec848e
Sayyed Raza <razsoriginals@gmail.com>
2024-01-13 16:34:00 +0530

    Updated Navigation

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-// App.js
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './lib/screens/home/HomeScreen';
+import ArticlePage from './lib/screens/articles/post/ArticlePage';
 
-const App = () => {
-  return <HomeScreen />;
-  // need to add navigation stack
+const Stack = createNativeStackNavigator();
+
+const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Article" component={ArticlePage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
-export default App;
+export default Navigation;
